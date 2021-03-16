fcce31f53941e0c2c4dea34fb410d893a618cdbf
Mirela <adisserdarevic@gmail.com>
2021-03-16 17:45:57 +0100

    Added file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+function myTrueBoolean() {
+    // Only change code below this line
+    
+    return true;
+    
+    // Only change code above this line
+}
+
+
+console.log(myTrueBoolean());
+module.exports = myTrueBoolean;
