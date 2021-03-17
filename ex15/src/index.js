e02d4d1dd969e112602a7f40d9b6d33c8ca87561
Mirela <adisserdarevic@gmail.com>
2021-03-17 01:17:52 +0100

    Added file index.js

---
+++ ex15/src/index.js
@@ -0,0 +1,25 @@
+function myDoWhile() {
+    var myNumbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+    var i = 1;
+    const n = 9;
+    
+// Only change below above this line
+
+do {
+    console.log();
+    break;
+} while (i <= n); {
+   
+    i++;
+    
+}
+return myNumbers;
+
+}    
+// Only change code above this line
+
+
+console.log(myDoWhile());
+
+
+module.exports = myDoWhile;
